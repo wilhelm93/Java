20dccf6fa03e4fd93af79b418e91cb240b62fd8f
wilhelm93 <73252790+wilhelm93@users.noreply.github.com>
2026-06-21 19:53:28 +0200

    added 4 functionable options

--- toDoList.java
+++ toDoList.java
@@ -1,9 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class toDoList {
-    public static void main (String [] args) {
 
-        
+    public static List<String> taskList = new ArrayList<>();
 
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the toDolist");
+        Scanner scanner = new Scanner(System.in);
+
+        boolean running = true;
+
+        while (running){
+            System.out.println("Please select an option:");
+            System.out.println("1. Add task");
+            System.out.println("2. List tasks");
+            System.out.println("3. Remove task");
+            System.out.println("4. Exit");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            String taskName = "";
+
+            switch (option) {
+                case 1:
+                    System.out.println("Please add task's name: ");
+                    taskName = scanner.nextLine();
+                    String result = addTask(option, taskName);
+                    System.out.println(result);
+                    break;
+
+                case 2:
+                    String listResult = listAddedTasks(option, taskName);
+                    break;
+
+                case 3:
+                    System.out.println("Which task should be removed?");
+                    String taskToRemove = scanner.nextLine();
+                    System.out.println(removeTask(taskToRemove));
+                    break;
+
+                case 4:
+                    System.out.println("Exiting the programm...");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please choose 1-4");
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+        public static String addTask ( int option, String taskName){
+            taskList.add(taskName);
+            return "Task added: " + taskName + "\n" + "----------";
+        }
+        public static String listAddedTasks ( int option, String taskName){
+            System.out.println("--- Current Tasks ---");
+            for (String element : taskList) {
+                System.out.println("-> " + element);
+            }
+            return ""; // Return empty string since we printed directly
+        }
+
+        public static String removeTask (String taskName) {
+            System.out.println("Removing task...");
+            taskList.remove(taskName);
+            System.out.println("Task removed: " + taskName + "\n" + "----------");
+            return  "";
+        }
 
     }
-}
