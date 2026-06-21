2ad2cdad41f4d8eb20fce0008c2ef7ee5e255907
wilhelm93 <73252790+wilhelm93@users.noreply.github.com>
2026-06-21 15:37:47 +0200

    Create calculator_methods+switchCase.java
    added methods and switch cases

---
+++ calculator_methods+switchCase.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class calculator_Methods {
+    public static void main (String [] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Bitte geben Sie die erste Zahl ein: ");
+    double a = scanner.nextDouble();
+    System.out.println("Bitte geben Sie die zweite Zahl ein: ");
+    double b = scanner.nextDouble();
+
+    System.out.println("Bitte geben Sie die Operation ein: ");
+
+    char Antwort = scanner.next().charAt(0);
+
+    switch (Antwort) {
+        case '+':
+            System.out.println("Antwort: " + addition(a, b));
+            break;
+        case '-':
+            System.out.println("Antwort: " + subtraktion(a, b));
+            break;
+        case '/':
+            System.out.println("Antwort: " + division(a, b));
+            break;
+        case '*':
+            System.out.println("Antwort: " + multiplikation(a, b));
+            break;
+        default:
+            System.out.println("Bitte geben Sie eine gültige Operation ein. +, -, / oder *");
+    }
+
+    }
+
+//methods: Addition, Subtraktion, Division, Multiplikation
+    public static double addition (double a, double b) {
+        return a + b;
+    }
+    public static double subtraktion (double a, double b) {
+        return a - b;
+    }
+    public static double division (double a, double b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
+    public static double multiplikation (double a, double b){
+        return a * b;
+    }
+}
